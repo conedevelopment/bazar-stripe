3c101b8f847855f24fc2730deff558d4a18b8796
D. Nagy Gergő <hello@iamgergo.com>
2024-02-06 09:26:28 +0100

    wip

--- src/Listeners/HandlePaymentIntentSucceededEvent.php
+++ src/Listeners/HandlePaymentIntentSucceededEvent.php
@@ -6,6 +6,7 @@ use Cone\Bazar\Models\Order;
 use Cone\Bazar\Models\Transaction;
 use Cone\Bazar\Stripe\Events\StripeWebhookInvoked;
 use Cone\Bazar\Support\Facades\Gateway;
+use Illuminate\Support\Facades\Date;
 use Stripe\Event;
 use Throwable;
 
@@ -16,23 +17,29 @@ class HandlePaymentIntentSucceededEvent
      */
     public function handle(StripeWebhookInvoked $event): void
     {
+        $order = Order::proxy()
+            ->newQuery()
+            ->where('uuid', $event->event->data['object']['metadata']['order'])
+            ->firstOrFail();
+
         switch ($event->event->type) {
+            case 'charge.refunded':
+                $this->handleRefund($event->event, $order);
+                break;
             case 'payment_intent.succeeded':
-                $this->completeTransaction($event->event);
+                $this->handlePayment($event->event, $order);
                 break;
         }
     }
 
     /**
-     * Handle payment.
+     * Handle the payment.
      */
-    public function completeTransaction(Event $event): void
+    public function handlePayment(Event $event, Order $order): void
     {
         try {
             $transaction = Transaction::proxy()->newQuery()->where('key', $event->data['object']['id'])->firstOrFail();
         } catch (Throwable $exception) {
-            $order = Order::proxy()->newQuery()->where('uuid', $event->data['object']['metadata']['order'])->firstOrFail();
-
             $transaction = Gateway::driver('stripe')->pay(
                 $order,
                 $event->data['object']['amount'] / 100,
@@ -41,5 +48,21 @@ class HandlePaymentIntentSucceededEvent
         }
 
         $transaction->markAsCompleted();
+    }
+
+    /**
+     * Handle the refund.
+     */
+    public function handleRefund(Event $event, Order $order): void
+    {
+        foreach ($event->data['refunds']['data'] as $refund) {
+            if (is_null($order->transactions->firstWhere('key', $refund['id']))) {
+                Gateway::driver('stripe')->refund(
+                    $order,
+                    $refund['amount'] / 100,
+                    ['key' => $refund['id'], 'completed_at' => Date::now()]
+                );
+            }
+        }
     }
 }

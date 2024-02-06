56b7f57726149d8d22e5618a7e185e9fad2fde1b
D. Nagy Gergő <hello@iamgergo.com>
2024-02-06 09:05:03 +0100

    wip

--- src/Listeners/HandlePaymentIntentSucceededEvent.php
+++ src/Listeners/HandlePaymentIntentSucceededEvent.php
@@ -6,6 +6,7 @@ use Cone\Bazar\Models\Order;
 use Cone\Bazar\Models\Transaction;
 use Cone\Bazar\Stripe\Events\StripeWebhookInvoked;
 use Cone\Bazar\Support\Facades\Gateway;
+use Stripe\Event;
 use Throwable;
 
 class HandlePaymentIntentSucceededEvent
@@ -15,17 +16,27 @@ class HandlePaymentIntentSucceededEvent
      */
     public function handle(StripeWebhookInvoked $event): void
     {
-        $stripeEvent = $event->event;
+        switch ($event->event->type) {
+            case 'payment_intent.succeeded':
+                $this->completeTransaction($event->event);
+                break;
+        }
+    }
 
+    /**
+     * Handle payment.
+     */
+    public function completeTransaction(Event $event): void
+    {
         try {
-            $transaction = Transaction::proxy()->newQuery()->where('key', $stripeEvent->data['object']['id'])->firstOrFail();
+            $transaction = Transaction::proxy()->newQuery()->where('key', $event->data['object']['id'])->firstOrFail();
         } catch (Throwable $exception) {
-            $order = Order::proxy()->newQuery()->where('uuid', $stripeEvent->data['object']['metadata']['order'])->firstOrFail();
+            $order = Order::proxy()->newQuery()->where('uuid', $event->data['object']['metadata']['order'])->firstOrFail();
 
             $transaction = Gateway::driver('stripe')->pay(
                 $order,
-                $stripeEvent->data['object']['amount'] / 100,
-                ['key' => $stripeEvent->data['object']['id']]
+                $event->data['object']['amount'] / 100,
+                ['key' => $event->data['object']['id']]
             );
         }
 
